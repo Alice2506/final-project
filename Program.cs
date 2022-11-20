85c9cb7cbc6454461ac858c2aa7ce81e6baccd4a
Alice2506 <ralko84@mail.ru>
2022-11-20 17:32:11 +0300

    added corrections

--- Program.cs
+++ Program.cs
@@ -2,25 +2,6 @@
 // меньше либо равна трем символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте 
 // выполнения алгоритма.
 
-// Console.WriteLine("Введите количество строк массива: ");
-// int count = int.Parse(Console.ReadLine());
-// string[] arrayOne = new string[count];
-// Console.WriteLine("Введите данные: ");
-// for (int i = 0; i < arrayOne.Length; i++)
-// {
-//     arrayOne[i] = Console.ReadLine();
-// }
-// // запись данных в новый массив
-// string[] resultArray = new string[count];
-// for (int i = 0; i < arrayOne.Length; i++)
-// {
-//     if (arrayOne[i].Length <= 3)
-//     {
-//         arrayOne[i] = resultArray[i];
-//     }
-//     Console.WriteLine(resultArray[i]);
-// }
-
 Console.WriteLine("Введите количество строк массива: ");
 int size = int.Parse(Console.ReadLine());
 string[] arrayOne = new string[size];
